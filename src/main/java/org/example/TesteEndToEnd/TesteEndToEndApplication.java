dcf7bc8a2f93cbc767482a4caff4c25e7ecd56cb
Paulo Hirata <paulo.hirata@dextra-sw.com>
2021-05-23 12:15:14 -0300

    Criacao do projeto

---
+++ src/main/java/org/example/TesteEndToEnd/TesteEndToEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.TesteEndToEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEndToEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEndToEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/TesteEndToEnd/TesteEndToEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.TesteEndToEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEndToEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

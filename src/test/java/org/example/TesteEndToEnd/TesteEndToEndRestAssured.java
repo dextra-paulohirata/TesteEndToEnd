d4b2aafa9bcf6ae60bd13691afda65d0b3230b71
Paulo Hirata <paulo.hirata@dextra-sw.com>
2021-06-16 22:13:54 -0300

    Inclusao dos testes com rest assured

--- src/test/java/org/example/TesteEndToEnd/TesteEndToEndRestAssured.java
+++ src/test/java/org/example/TesteEndToEnd/TesteEndToEndRestAssured.java
@@ -1,7 +1,10 @@
 package org.example.TesteEndToEnd;
 
+import com.google.gson.Gson;
 import io.restassured.RestAssured;
 
+import org.example.TesteEndToEnd.dto.UsuarioDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,18 +23,54 @@ public class TesteEndToEndRestAssured {
     }
 
     @Test
-    public void usuario_controller_returns_200_with_expected_usuario() {
-        when().get("/usuarios/{id}", 1).
-                then().
-                statusCode(200).
-                body("id", equalTo(1),
-                        "nome", equalTo("Phoenix"));
+    public void givenUsuarioThenSavesUsuarioAndDelete() {
+        String usuarioCreated = given().contentType("application/json")
+                .body(new UsuarioDTO(2L, "Typhon", "01500-060", "Rua Vazia", "144", "", "Sao Paulo", "SP"))
+                .when().post("usuarios").asString();
+
+        Gson gson = new Gson();
+        UsuarioDTO dto = gson.fromJson(usuarioCreated, UsuarioDTO.class);
+
+        when().delete("/usuarios/{id}", dto.getId()).then().statusCode(200);
+
+        Assertions.assertEquals("Typhon", dto.getNome());
+        Assertions.assertEquals("Rua Vazia", dto.getLogradouro());
     }
 
     @Test
-    public void usuario_controller_returns_200_with_all_usuario() {
+    public void givenUsuarioThenFindByIdAndDelete() {
+        String usuarioCreated = given().contentType("application/json")
+                .body(new UsuarioDTO(1L, "Phoenix", "01234-010", "Rua Nomade", "365", "ap 12", "Sao Paulo", "SP"))
+                .when().post("usuarios").asString();
+
+        Gson gson = new Gson();
+        UsuarioDTO dto = gson.fromJson(usuarioCreated, UsuarioDTO.class);
+
+        when().get("/usuarios/{id}", dto.getId()).
+                then().
+                statusCode(200).
+                body("nome", equalTo(dto.getNome()));
+
+        when().delete("/usuarios/{id}", dto.getId()).
+                then().statusCode(200);
+    }
+
+    @Test
+    public void givenUsuarioThenFindAllUsuarioAndDelete() {
+        String usuarioCreated = given().contentType("application/json")
+                .body(new UsuarioDTO(1L, "Dallas", "02458-000", "Av Prates", "740", "ap 104", "Sao Paulo", "SP"))
+                .when().post("usuarios").asString();
+
+        Gson gson = new Gson();
+        UsuarioDTO dto = gson.fromJson(usuarioCreated, UsuarioDTO.class);
+
         when().get("/usuarios").
                 then().
                 statusCode(200).body("", hasSize(1));
+
+        when().delete("/usuarios/{id}", dto.getId()).
+                then().statusCode(200);
     }
+
+
 }
